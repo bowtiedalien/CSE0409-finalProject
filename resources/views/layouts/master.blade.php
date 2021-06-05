34b52347b39f8bae7532cb6441485c463d99b256
sarah <sarah.ahmed@freedomdev.com>
2021-06-05 10:35:27 +0300

    create main template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <title>Sara Ahmed - Student Management System</title>
+    <style>
+        @import url(//fonts.googleapis.com/css?family=Lato:700);
+    </style>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
