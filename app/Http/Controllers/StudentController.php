143fb3df9dc99c117270bf7576090ecc16473ea0
sarah <sarah.ahmed@freedomdev.com>
2021-06-05 10:46:11 +0300

    function to save student info

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     //
+    function save(Request $req)
+    {
+        $req->validate([
+            'name' => 'required',
+            'surname' => 'required',
+            'department' => 'required'
+        ]);
+
+        $admin = new Student();
+        $admin->name = $req->name;
+        $admin->surname = $req->surname;
+        $admin->department = $req->department;
+        $save = $admin->save();
+
+        if ($save) {
+            return back()->with('success', 'Student created successfully');
+        } else {
+            return back()->with('failure', 'Something went wrong, please try again');
+        }
+    }
 }
