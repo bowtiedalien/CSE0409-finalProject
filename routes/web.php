3e89a6fdbba8ef0d6f4f8ac017fd5f89b04db17e
sarah <sarah.ahmed@freedomdev.com>
2021-06-05 10:27:02 +0300

    edit student info route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/student/edit', [StudentController::class, 'editInfo'])->name('student.edit');
